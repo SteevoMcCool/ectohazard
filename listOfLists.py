343b2984859f767c1fb1c098f26512a5416b7647
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2026-03-12 16:39:11 -0400

    optimizer for combining multiple lists without copying

---
+++ listOfLists.py
@@ -0,0 +1,30 @@
+# Allows the combination of several lists without copying
+class ListOfLists: 
+    def __init__(self,*args):
+        for arg in args:
+            assert(type(arg) == list)
+        self.lists = [arg for arg in args]
+    
+    def __len__(self):
+        return sum(len(L) for L in self.lists)
+
+    def __iter__(self):
+        for L in self.lists:
+            yield from L 
+        
+    
+    def __getitem__(self, key):
+        if (type(key) == int):
+            for L in self.lists:
+                if key < len(L):
+                    return L[key]
+                key -= len(L)
+            raise Exception("Index out of bounds")
+        else:
+            raise Exception("Index must be integer")
+        
+    def __setitem__(self, key, value):
+        raise Exception("ListOfLists Class does not modify original lists!")       
+
+
+    
