a407d95cc8dae04030301a1e98dbfa91cbfd69a1
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2026-03-12 21:05:31 -0400

    greatly reduced fisheye, improved turn sensitivity

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ dt = 0
 
 class Player: 
     def __init__(self):
-        self.camera = Camera(Ray(Vector2(0,0),0),1.5)
+        self.camera = Camera(Ray(Vector2(0,0),0),1)
         self.controller = Controller()
         self.speed = 10
         self.area = 135
         self.controller.addBind(K_w,whileDown= lambda dt: self.move(dt*self.speed* self.camera.center.lookVector()))
         self.controller.addBind(K_s,whileDown= lambda dt: self.move(-dt*self.speed* self.camera.center.lookVector()))
 
-        self.controller.addBind(K_d,whileDown= lambda dt: self.turn(0.5* dt) )
-        self.controller.addBind(K_a,whileDown= lambda dt: self.turn(0.5* -dt))
+        self.controller.addBind(K_d,whileDown= lambda dt: self.turn(0.85* dt) )
+        self.controller.addBind(K_a,whileDown= lambda dt: self.turn(0.85* -dt))
     def move(self,deltaPos):
         self.camera.center.pos += deltaPos
     def turn(self,deltaAngle):
         self.camera.center.angle =  (self.camera.center.angle  + deltaAngle) % (6.28318)
 
-A,B = 2,2
+A,B = 0,0
 player = Player()
 areas = AreaLoader() 
 
