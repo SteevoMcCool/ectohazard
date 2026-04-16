55a06e6337ac449d5ff23635c1810575675318ee
Josiahye <xinye0032@gmail.com>
2026-04-16 17:16:05 -0400

    Update gameStateLoader.py

--- gameStateLoader.py
+++ gameStateLoader.py
@@ -1,9 +1,39 @@
-def load(directory:str):
-    #loads the game from the current directory
-    # 'directory' is a fancy word for folder
-    # returns: tuple playerData, centerArea, 
-    pass
+import pickle
+import os
 
 
-def unload(directory:str):
-    pass
+def load(directory: str):
+    """
+    load file from directory
+    """
+    file_path = os.path.join(directory, "savegame.dat")
+
+    if not os.path.exists(file_path):
+        print(f"File not exist: {file_path}")
+        return None, None
+
+    with open(file_path, "rb") as f:
+        try:
+            data = pickle.load(f)
+            playerData = data.get("player")
+            centerArea = data.get("area")
+            return playerData, centerArea
+        except Exception :
+            print(f"Load file failed: {Exception}")
+            return None, None
+
+
+def unload(directory: str, playerData, centerArea):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_path = os.path.join(directory, "savegame.dat")
+
+    data_to_save = {
+        "player": playerData,
+        "area": centerArea}
+
+    with open(file_path, "wb") as f:
+        pickle.dump(data_to_save, f)
+
+    print(f"File saved to: {file_path}")
