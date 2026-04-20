833b81b546f89611bbc4b57c7c2ec1de88866357
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2026-04-20 10:29:52 -0400

    moved the "getFile" method

---
+++ gamepaths.py
@@ -0,0 +1,41 @@
+# Globals that hold save files
+CORE_FILE_PATH = "./GameCoreFiles"
+SAVES_FILE_PATH = "./GameSaves"
+
+# Globals that hold gamefile paths
+AREA_PATH = f"{CORE_FILE_PATH}/Areas"
+TEXTURE_PATH = f"{CORE_FILE_PATH}/Textures"
+DIALOGUE_PATH = f"{CORE_FILE_PATH}/Dialogues"
+ENTITIES_PATH = f"{CORE_FILE_PATH}/Entities"
+
+
+
+
+import os
+FALLBACK_DIRECTORY = "GameCoreFiles"
+def getFile(directory:str|os.PathLike,fname:str,fileType:str):
+    fileType = fileType.lower()
+    validFiletypes = ["area","dialogue","entity","texture"]
+    if fileType not in validFiletypes:
+        print(f"Error: {fileType} is invalid, expected one of {validFiletypes}")
+        exit(1) 
+    
+    match fileType:
+        case "area":
+            midpart = "Areas"
+        case "dialogues":
+            midpart = "Dialogues"
+        case "entity":
+            midpart = "Entities"
+        case "texture":
+            midpart = "Texture"
+    fullPath = os.path.join(directory,midpart,fname)
+    if os.path.exists(fullPath) and os.path.isfile(fullPath):
+        return fullPath
+    
+    fullPath = os.path.join(FALLBACK_DIRECTORY,midpart,fname)
+    if os.path.exists(fullPath) and os.path.isfile(fullPath):
+        return fullPath
+
+    print(f"Error: cannot find {fileType} of name {fname} in {directory} or in fallback={FALLBACK_DIRECTORY}")
+    exit(1)
