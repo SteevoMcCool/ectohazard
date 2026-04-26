d9807eb82ea5bb6e71695d0df4cbce68470e49c0
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2026-04-26 13:15:33 -0400

    added some templates to make entity and item behaviors

---
+++ EntityBehaviors/Albert.py
@@ -0,0 +1,28 @@
+def update(self,areaID,player,others): 
+    """
+        Function to be called every tick
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that this entity is bound to
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+
+
+def approached(self,approacher,areaID,player,others):
+    """
+        Function to be called when another entity|player gets close
+        Parameters:
+            self: the Entity described by the file 
+            approacher: the Entity|Player that approached 
+            areaID: the Int id of the area that this entity is bound to
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+
---
+++ ItemBehaviors/PlasmaRay.py
@@ -0,0 +1,66 @@
+def update(self,areaID,player,others): 
+    """
+        Function to be called every tick
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that is currently loaded 
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+
+def button1down(self,areaID,player,others):
+    """
+        Function to be called when the player presses button 1 (defaults to minus key)
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that is currently loaded 
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+def button1up(self,areaID,player,others):
+    """
+        Function to be called when the player presses button 1 (defaults to minus key)
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that is currently loaded 
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+
+def button2down(self,areaID,player,others):
+    """
+        Function to be called when the player presses button 2 (defaults to plus key)
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that is currently loaded 
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+def button2up(self,areaID,player,others):
+    """
+        Function to be called when the player presses button 2 (defaults to plus key)
+        Parameters:
+            self: the Entity described by the file 
+            areaID: the Int id of the area that is currently loaded 
+            player: the Player
+            others: list of other Entities that are loaded
+            walls:  nearby walls
+    """
+    pass
+
+
+
+
+
