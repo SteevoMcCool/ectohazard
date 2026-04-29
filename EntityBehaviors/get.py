b24bc6a6487e96cb3e2b145118e56168240fc43c
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2026-04-29 19:48:47 -0400

    okay enough for the demo

--- EntityBehaviors/get.py
+++ EntityBehaviors/get.py
@@ -1,12 +1,16 @@
-import EntityBehaviors.Albert, EntityBehaviors.Blank, EntityBehaviors.BasicEnemy, EntityBehaviors.Portal, EntityBehaviors.Peeker
+import EntityBehaviors.Albert, EntityBehaviors.Blank, EntityBehaviors.BasicEnemy, EntityBehaviors.Portal, EntityBehaviors.Peeker,EntityBehaviors.HalfApparation,EntityBehaviors.Mouther
 def behavior(name:str):
     match name:
         case "Albert":
             return EntityBehaviors.Albert
         case "Erdon":
             return EntityBehaviors.BasicEnemy
+        case "HalfApparation":
+            return EntityBehaviors.HalfApparation
         case "Portal":
             return EntityBehaviors.Portal
         case "Peeker":
             return EntityBehaviors.Peeker
+        case "Mouther":
+            return EntityBehaviors.Mouther
     return EntityBehaviors.Blank
